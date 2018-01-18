b447ecdf307a9336df7d4a73db5ff7bdb88edb68
wang198927 <wang198927@163.com>
2018-01-18 22:33:39 +0800

    完成薪资计算后台逻辑，还需完成EXCEL下载

--- application/admin/controller/SalaryController.php
+++ application/admin/controller/SalaryController.php
@@ -33,47 +33,85 @@ class SalaryController extends CommonController
         $rows = $_POST['rows'];
         $page = $_POST['page'];
         $path = $this->getDataByCampusid($_POST);
-        /*        $searchPath = $this->searchNotLike($path,$_POST,'course_grade_id','course_subject_id');
+/*                $searchPath = $this->searchNotLike($path,$_POST,'teacher_name','starttime','endtime');
                 if(isset($searchPath['campusid'])){
                     $searchPath['course.campusid'] = $searchPath["campusid"];
                     unset($searchPath["campusid"]);
                 }*/
-        $salaryinfos = Db::query('select a.id ,a.teacher_name,sum(c.records_classhour) as classhour,d.salarytemp_name,d.salarytemp_newbonus,d.salarytemp_demobonus from ew_teacher a 
-left join ew_classrecords c on a.id = c.records_teacherid
+        unset($_POST['name']);
+        //查每个员工基本工资
+        $salaryinfos = Db::query('select a.id as teacher_id,a.teacher_name,d.* from ew_teacher a 
 left join ew_salarytemp d on a.teacher_salarytemp_id = d.id
-group by a.id');
-        $salesinfos = Db::query('select a.id ,c.sales_ordertypename,sum(c.sales_money) as money from ew_teacher a 
+group by a.id
+order by a.id
+limit :offset,:row',['offset'=>$rows * ($page - 1),'row'=>$rows]);
+        //获取总行数
+        $totalrows=sizeof(Db::query('select a.id as teacher_id,a.teacher_name,d.* from ew_teacher a 
+left join ew_salarytemp d on a.teacher_salarytemp_id = d.id
+group by a.id
+order by a.id'));
+
+        //查每个员工课时数
+        $classsalaryinfos = Db::query('select a.id as teacher_id,a.teacher_name,sum(c.records_classhour) as classhour from ew_teacher a 
+left join ew_classrecords c on a.id = c.records_teacherid
+where c.records_endtime between :starttime and :endtime
+group by a.id
+order by a.id',['starttime'=>$_POST['starttime'],'endtime'=>$_POST['endtime']]);
+
+        //查每个员工各类销售额
+        $salesinfos = Db::query('select a.id ,c.sales_ordertypename,sum(c.sales_money*0.1) as money from ew_teacher a 
 left join ew_salesrecord c on a.id = c.sales_teacherid
-group by a.id,c.sales_ordertypename');
+where c.sales_day between :starttime and :endtime
+group by a.id,c.sales_ordertypename',['starttime'=>$_POST['starttime'],'endtime'=>$_POST['endtime']]);
+
           for ($i=0;$i<sizeof($salaryinfos);$i++)
           {
+              $salaryinfos[$i]['classhour_money']=0;
+              //计算课时费
+              for($k=0;$k<sizeof($classsalaryinfos);$k++)
+              {
+                  //遍历找到课时查询结果中该员工的课时数，计算课时费
+                  if($salaryinfos[$i]['teacher_id'] ==$classsalaryinfos[$k]['teacher_id'] )
+                  {
+                      $salaryinfos[$i]['classhour_money']=$classsalaryinfos[$k]['classhour']*$salaryinfos[$i]['salarytemp_classhour'];
+                  }
+              }
+              $salaryinfos[$i]['新单']=0;
+              $salaryinfos[$i]['demo课']=0;
+              //计算不包含销售提成的总工资
+              $salaryinfos[$i]['total']=$salaryinfos[$i]['salarytemp_base']+$salaryinfos[$i]['salarytemp_pos']+$salaryinfos[$i]['salarytemp_com']+$salaryinfos[$i]['salarytemp_trans']+
+              $salaryinfos[$i]['salarytemp_food']+$salaryinfos[$i]['classhour_money'];
+              //循环每个员工计算各类销售提成
               for ($j=0;$j<sizeof($salesinfos);$j++)
               {
-                  if($salaryinfos[$i]['id'] ==$salesinfos[$j]['id'] )
+                  if($salaryinfos[$i]['teacher_id'] ==$salesinfos[$j]['id'] )
                   {
                       $tempordertype = $salesinfos[$j]['sales_ordertypename'];
                       $salaryinfos[$i][$tempordertype]=$salesinfos[$j]['money'];
                       if($tempordertype=='新单')
                       {
-                          $salaryinfos[$i][$tempordertype]=$salaryinfos[$i][$tempordertype]*$salaryinfos[$i]['salarytemp_newbonus']/100;
+                          $salaryinfos[$i][$tempordertype]=$salaryinfos[$i][$tempordertype]*$salaryinfos[$i]['salarytemp_newbonus']/100;  //提成比例需要换成%
+                          $salaryinfos[$i]['total']=$salaryinfos[$i]['total']+$salaryinfos[$i][$tempordertype];
                       }
                       elseif ($tempordertype=='demo课')
                       {
                           $salaryinfos[$i][$tempordertype]=$salaryinfos[$i][$tempordertype]*$salaryinfos[$i]['salarytemp_demobonus']/100;
+                          $salaryinfos[$i]['total']=$salaryinfos[$i]['total']+$salaryinfos[$i][$tempordertype];
+                      }
+                      else
+                      {
+                          //todo 其他销售类统计
                       }
                   }
               }
           }
 //        $temp1 = $salaryinfos[0]['teacher_name'];
 //        $temp2 = $salaryinfos[0]['salarytemp']['salarytemp_name'];
-        $total = sizeof($salaryinfos);
 
-        $data['total'] = $total;
+        $data['total'] = $totalrows;
         $data['rows'] = $salaryinfos;
         return json_encode($data);
     }
-
-
 
 
     /**
@@ -89,10 +127,10 @@ group by a.id,c.sales_ordertypename');
             $searchPath['course.campusid'] = $searchPath["campusid"];
             unset($searchPath["campusid"]);
         }*/
-        $salarytemps = Salarytemp::where($path)->limit($rows * ($page - 1), $rows)->select();
+        $salaryTemps = Salarytemp::where($path)->limit($rows * ($page - 1), $rows)->select();
         $total = Salarytemp::where($path)->count();
         $data['total'] = $total;
-        $data['rows'] = $salarytemps;
+        $data['rows'] = $salaryTemps;
         return json_encode($data);
     }
     /**
--- static/js/ajax/salarycal.js
+++ static/js/ajax/salarycal.js
@@ -32,7 +32,7 @@ $(function () {
      */
     $('#datatable_salaryinfo').datagrid({
         striped: true,
-        //idField:'id',
+        //idField:'teacher_id',
         remoteSort: false,
         collapsible: true,
         fit: false,
@@ -44,25 +44,25 @@ $(function () {
         pageSize: psval,
         pageList: [psval, psval * 2, psval * 3, psval * 4, psval * 5],
         columns: [[{
-            field: 'salaryinfo_name',
+            field: 'teacher_name',
             title: '员工姓名',
             align: 'center',
             width: cellwidth,
             sortable: true
         }, {
-            field: 'salaryinfo_name',
+            field: 'total',
             title: '合计',
             align: 'center',
             width: cellwidth,
             sortable: true
         }, {
-            field: 'salaryinfo_base',
+            field: 'salarytemp_base',
             title: '基本工资',
             align: 'center',
             width: cellwidth,
             sortable: true
         }, {
-            field: 'salaryinfo_pos',
+            field: 'salarytemp_pos',
             title: '岗位工资',
             align: 'center',
             width: cellwidth,
@@ -71,36 +71,43 @@ $(function () {
                 return value.subject_name;
             }*/
         }, {
-            field: 'salaryinfo_com',
+            field: 'salarytemp_com',
             title: '通讯补贴',
             align: 'center',
             width: cellwidth,
             sortable: true
         },{
-            field: 'salaryinfo_trans',
+            field: 'salarytemp_trans',
             title: '交通补贴',
             align: 'center',
             width: cellwidth,
             sortable: true
         },{
-            field: 'salaryinfo_food',
+            field: 'salarytemp_food',
             title: '餐补',
             align: 'center',
             width: cellwidth,
             sortable: true
         },{
-            field: 'salaryinfo_classhour',
+            field: 'classhour_money',
             title: '课时费',
             align: 'center',
             width: cellwidth,
             sortable: true
         },{
-            field: 'salaryinfo_newbonus',
-            title: '提成',
+            field: '新单',
+            title: '新单提成',
             align: 'center',
             width: cellwidth,
             sortable: true
-            }
+        },{
+            field: 'demo课',
+            title: 'demo课提成',
+            align: 'center',
+            width: cellwidth,
+            sortable: true
+        }
+
 	/**	,{
       *      field: 'campus',
       *      title: '所在分区',
@@ -120,14 +127,23 @@ $(function () {
 
     //计算全部员工薪资
     $('#CalAll').click(function () {
+        var searchParams = getSearchParams();
+        //必须选择薪资计算起始、终止时间
+        if(searchParams['starttime']==undefined||searchParams['starttime']==''||searchParams['endtime']==undefined||searchParams['endtime']==''){
+            $.TeachDialog({
+                content: '请选择起始日期和终止日期',
+
+            });
+            return;
+        }
         $('#datatable_salaryinfo').datagrid({url:'admin/Salary/getSalaryInfo'});
         $('#datatable_salaryinfo').datagrid('load');
     })
-    //计算符合条件的员工薪资
+/*    //计算符合条件的员工薪资
     $('#CalPerson').click(function () {
-        $('#datatable_salaryinfo').datagrid({url:'admin/Salary/getSalaryInfo'});
+        $('#datatable_salaryinfo').datagrid({url:'admin/Salary/getPersonSalaryInfo'});
         $('#datatable_salaryinfo').datagrid('load');
-    })
+    })*/
 
 
 
